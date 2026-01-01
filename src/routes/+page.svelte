fc975ad03144057057a6bc4118ffa28f8bc9f485
uimaxbai <61615730+uimaxbai@users.noreply.github.com>
2026-01-01 15:39:45 +0000

    mess around a bit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,34 @@
 <style lang="scss">
 @font-face {
-  font-family:'JetBrains Mono';
+  font-family: 'JetBrains Mono';
   src: url(/fonts/JetBrainsMono-Bold.woff2);
   font-weight: bold;
   font-display: swap;
 }
 @font-face {
-  font-family:'JetBrains Mono';
+  font-family: 'JetBrains Mono';
   src: url(/fonts/JetBrainsMono-Regular.woff2);
+  font-weight: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family:'Inter';
+  src: url(/fonts/Inter-Bold.woff2);
+  font-weight: bold;
+  font-display: swap;
+}
+@font-face {
+  font-family:'Inter';
+  src: url(/fonts/Inter-Regular.woff2);
   font-weight: normal;
   font-display: swap;
 }
 
 * {
   user-select: none;
+  font-variant-numeric: tabular-nums;
+  font-feature-settings: "tnum";
 }
 
 .timeStr {
@@ -44,7 +59,6 @@ time div {
 :global(body) {
   color: #000;
   background: #fff;
-  font-family: 'JetBrains Mono', monospace;
 }
 
 :global(.light) {
@@ -97,7 +111,7 @@ time div {
   }
 
   .tower-cell {
-    font-size: 5vw;
+    width: 5vw;
   }
   
 }
@@ -118,6 +132,7 @@ time div {
   border-radius: 5px;
   border: 1px solid lightgray;
   transition: .2s;
+  margin-left: calc(-1rem - 24px);
 }
 .info-div {
   margin-top: -1em;
@@ -185,8 +200,19 @@ time div {
   font-size: 1vw;
 }
 
+.colon {
+  font-variant-numeric: normal;
+  font-feature-settings: normal;
+}
+
 @media (orientation: portrait) {
-  .colon, .dot {
+  .colon {
+    display: none;
+  }
+  .dateString span {
+    font-size: 2vh;
+  }
+  .footer {
     display: none;
   }
   div.time-container {
@@ -206,6 +232,13 @@ time div {
   }
 }
 
+.mono {
+  font-family: 'JetBrains Mono', monospace!important;
+}
+.sans {
+  font-family: 'Inter', sans-serif!important;
+}
+
 </style>
 
 
@@ -221,10 +254,9 @@ time div {
   <meta name="og:site_name" content="The Time - exact time, to the second" />
   <meta name="og:locale" content="en_GB" />
   <meta name="og:locale:alternate" content="en_US" />
-  <meta name="twitter:card" content="summary_large_image" />
   <meta name="twitter:title" content="The Time - exact time, to the second" />
   <meta name="twitter:description" content="A simple website that shows the time, to a tenth of a second. Night mode included as default. Custom themes. Open source. Clean interface. Timezones. And much more." />
-  <meta name="twitter:creator" content="@uimaxbai" />
+  <meta name="twitter:creator" content="@binnymum" />
   <meta name="robots" content="index, follow, max-snippet: -1, max-image-preview:large, max-video-preview: -1">
   <meta name="theme-color" content="#111111">
   <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png">
@@ -234,24 +266,24 @@ time div {
   <link rel="shortcut icon" href="/icons/favicon.ico">
   <meta name="msapplication-TileColor" content="#2d89ef">
   <meta name="msapplication-config" content="/icons/browserconfig.xml">
-  <link rel="canonical" href="https://time.okit.works">
-  <meta name="og:image" content="https://time.okit.works/icons/opengraph.png">
+  <link rel="canonical" href="https://time.binimum.org">
+  <meta name="og:image" content="https://time.binimum.org/icons/opengraph.png">
   <meta name="og:image:alt" content="The Time">
-  <meta name="twitter:image" content="https://time.okit.works/icons/opengraph.png">
+  <meta name="twitter:image" content="https://time.binimum.org/icons/opengraph.png">
   <script type="application/ld+json">
     {
       "@context" : "https://schema.org",
       "@type" : "WebSite",
       "name" : "The Time",
       "alternateName" : "Time",
-      "url" : "https://time.okit.works/"
+      "url" : "https://time.binimum.org/"
     }
   </script>
 </svelte:head>
 
 <Fullscreen>
   {#snippet children({ onToggle })}
-    <main class="main transition flex min-h-screen flex-col items-center justify-center p-4" class:dark={theme === 1} class:light={theme === 0} style="background: {hex}; color: rgba({rgb.r}, {rgb.g}, {rgb.b}, {rgb.a});">
+    <main class="main transition flex min-h-screen flex-col items-center justify-center p-4" class:sans={font === 1} class:mono={font === 0} class:dark={theme === 1} class:light={theme === 0} style="background: {hex}; color: rgba({rgb.r}, {rgb.g}, {rgb.b}, {rgb.a});">
         <div>
           <h1 class="timeStr">
             <time datetime={dateStr} class="timeEl flex flex-col">
@@ -264,11 +296,11 @@ time div {
                   <button transition:fade={{ delay: 0, duration: 200 }} aria-label="Show options" onclick={() => advancedShown = !advancedShown} class="toggleButton">
                     {#if advancedShown}
                       <div in:fade={{ delay: 0, duration: 200 }}>
-                        <Fa icon={faAngleUp} style="font-size: initial;" />
+                        <ChevronUp style="font-size: initial;" />
                       </div>
                     {:else}
                       <div in:fade={{ delay: 0, duration: 200 }}>
-                        <Fa icon={faAngleDown} style="font-size: initial;" />
+                        <ChevronDown style="font-size: initial;" />
                       </div>
                     {/if}
                   </button>
@@ -281,18 +313,20 @@ time div {
                   <span class="t s">{s}</span>
                   <span class="ms dot">.</span>
                   <span class='t ms'>{ms}</span>
+                  {#if syncing}
+                    <span id="tower-cell" class='tower-cell ml-2'><RefreshCw /></span>
+                  {:else}
+                    <span id="tower-cell" class='tower-cell ml-2'><RadioTower /></span>
+                  {/if}
                 </div>
-                {#if syncing}
-                  <span id="tower-cell" class='tower-cell ml-2'><Fa icon={faRotate} /></span>
-                {:else}
-                  <span id="tower-cell" class='tower-cell ml-2'><Fa icon={faTowerCell} /></span>
-                {/if}
+                <!--
                 {#if toggleShown}
                     <div transition:fade={{ delay: 0, duration: 200 }} class="techInfo" style="display: flex; flex-direction: column;">
                       <span class="delay">D:{diff}ms</span>
                       <span class="delay">L:{lastUpdated}</span>
                     </div>
                 {/if}
+                -->
                 </div>
             </time>
           </h1>
@@ -300,30 +334,37 @@ time div {
             <div class="gap-2 flex items-center">
               {#if advancedShown && toggleShown}
                 <div transition:fade={{ delay: 0, duration: 200 }} class="theme-selector">
-                  <button aria-label="Change to light mode" class="light" onclick={() => theme = 0}>{h}</button>
+                  <button aria-label="Change to light mode" id="update-storage" class="light" onclick={() => theme = 0}>{h}</button>
                   <span class="seperator">:</span>
-                  <button aria-label="Change to dark mode" class="dark" onclick={() => theme = 1}>{m}</button>
+                  <button aria-label="Change to dark mode" id="update-storage" class="dark" onclick={() => theme = 1}>{m}</button>
                   <span class="seperator">:</span>
-                  <button aria-label="Define a custom theme" class="custom" onclick={() => theme = 2}>{s}</button>
+                  <button aria-label="Define a custom theme" id="update-storage" class="custom" onclick={() => theme = 2}>{s}</button>
                 </div>
                 {#if theme === 2}
                   <ColorPicker
                     bind:hex
                     label=""
+                    id="update-storage"
                   />
                   <ColorPicker
                     bind:rgb
                     label=""
+                    id="update-storage"
                   />
                 {/if}
+                <div transition:fade={{ delay: 0, duration: 200 }} class="theme-selector">
+                  <button aria-label="Change to monospace font" id="update-storage" class="dark mono" onclick={() => font = 0}>{h}</button>
+                  <span class="seperator" style="margin-left: 0;">:</span>
+                  <button aria-label="Change to sans serif font" id="update-storage" class="dark sans" style="border-right: 0;" onclick={() => font = 1}>{m}</button>
+                </div>
                 <button class="fs-button" onclick={() => {fullscreen = !fullscreen; onToggle();}}>
                   {#if fullscreen}
-                    <Fa icon={faCompress} />
+                    <Minimize />
                   {:else if !fullscreen}
-                    <Fa icon={faExpand} />
+                    <Maximize />
                   {/if}
                 </button>
-                <span id="timezone" transition:fade={{ delay: 0, duration: 200 }} class="timezone">Timezone: {timezone}</span>
+                
               {/if}
             </div>
             {#if advancedShown && toggleShown}
@@ -332,7 +373,9 @@ time div {
           </div>
           {#if advancedShown && toggleShown}
             <div transition:fade={{ delay: 0, duration: 200 }} class="flex flex-col gap-2 mt-4">
+              <span id="timezone">Timezone: {timezone}</span>
               <span>Unix: <time id="unix" datetime={date.toTimeString()}>{date.getTime()}</time> </span>
+              <span>Drift: {diff}ms | Last updated: {lastUpdated}</span>
               <span>IP: {ip.ipAddress == "" || ip.ipAddress === undefined ? "None" : ip.ipAddress}</span>
               <span>Location: {ip.cityName}, {ip.regionName}, {ip.countryCode}</span>
             </div>
@@ -340,7 +383,13 @@ time div {
         </div>
         {#if toggleShown}
           <a class="footer" href="https://github.com/uimaxbai/time.okit.works" target="_blank">
-            <Fa icon={faGithub} />
+            {#if theme === 0}
+              <svg viewBox="0 0 98 96" style="width: 48px; height: 48px;" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="#24292f"/></svg>
+            {:else if theme === 1}
+              <svg viewBox="0 0 98 96" style="width: 48px; height: 48px;" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="#fff"/></svg>
+            {:else}
+              <svg viewBox="0 0 98 96" style="width: 48px; height: 48px;" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="#fff"/></svg>
+            {/if}
           </a>
         {/if}
     </main>
@@ -350,14 +399,13 @@ time div {
 
 <script lang="ts">
   import '../app.css';
-  import Fa from '../../node_modules/svelte-fa/dist/fa.svelte';
   import { fade } from 'svelte/transition';
-  import { faTowerCell, faAngleDown, faAngleUp, faExpand, faCompress, faRotate } from '@fortawesome/free-solid-svg-icons/index.js';
-  import { faGithub } from '@fortawesome/free-brands-svg-icons/index.js';
   import { onMount } from 'svelte';
   import Fullscreen from "$lib/Fullscreen.svelte";
   import ColorPicker from 'svelte-awesome-color-picker';
-  let theme = $state(1);
+  import { ChevronUp, ChevronDown, RefreshCw, RadioTower, Minimize, Maximize } from '@lucide/svelte';
+  let theme = $state(1); // 0 = light, 1 = dark, 2 = custom
+  let font = $state(0); // 0 = mono, 1 = sans
   let toggleShown = $state(true);
   let hex: string = $state("#000000ff");
   let lastUpdated = $state("Never");
@@ -390,37 +438,65 @@ time div {
     "language": "",
   });
   
-  // console.log(ip);
-  /* $effect(() => {
-    console.log(ip);
-  }) */
-  const myHeaders = new Headers();
-  myHeaders.append("Origin", "https://www.maxmind.com");
-  myHeaders.append("Referer", "https://www.maxmind.com");
-  myHeaders.append("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36");
-  // myHeaders.append("Cookie", "_cfuvid=zGcahtEdqkzsWjhQxwlXlF756GPN3O0HSKtl_tBmFp0-1750693233628-0.0.1.1-604800000");
-  const requestOptions = {
-    method: "GET",
-    headers: myHeaders,
-    redirect: "follow"
-  };
+  async function getGeoLocation() {
+    const endpoints = [
+      "https://apip.cc/json",
+      "https://ipapi.co/json/",
+      "https://reallyfreegeoip.org/json/",
+    ];
+
+    for (const url of endpoints) {
+      try {
+        const res = await fetch(url, { cache: "no-cache" });
+        if (!res.ok) throw new Error("Non-OK HTTP status");
+
+        const data = await res.json();
+        console.log("Geo success from", url, data);
+
+        // Map fields into your `ip` state object
+        ip = {
+          ipAddress: data.ip || data.query || ip.ipAddress,
+          latitude: data.latitude ?? data.lat ?? ip.latitude,
+          longitude: data.longitude ?? data.lon ?? ip.longitude,
+          countryName: data.CountryName || data.country_name || data.country || "",
+          countryCode: data.CountryCode || data.country_code || data.countryCode,
+          cityName: data.city || data.City || "",
+          regionName: data.RegionName || data.region || data.region_name || "",
+          zipCode: data.postal || data.zip || "",
+          timeZone: data.timezone || data.time_zone || "",
+          continent: data.continent_name || "",
+          continentCode: data.continent_code || "",
+          connectionType: data.connection_type ?? ip.connectionType,
+          accuracy: data.accuracy ?? ip.accuracy,
+          language: Array.isArray(data.languages)? data.languages.join(", ") : ""
+        };
+
+        // Done — stop trying more services
+        return;
+      } catch (err) {
+        console.warn(`Failed geo with ${url}`, err);
+      }
+    }
+
+    console.error("All geolocation attempts failed.");
+  }
+
+  function updateLocalStorage() {
+    localStorage.setItem("theme", theme.toString());
+    localStorage.setItem("font", font.toString());
+    if (theme === 2) {
+      localStorage.setItem("bg", hex);
+      localStorage.setItem("rgb", JSON.stringify(rgb));
+    }
+  }
 
   onMount(() => {
-    fetch("https://geoip.maxmind.com/geoip/v2.1/city/me", requestOptions).then((d) => {
-      return d.json()
-    }).then((d) => {
-      ip.ipAddress = d.traits.ip_address;
-      ip.cityName = d.city.names.en;
-      ip.latitude = d.location.latitude;
-      ip.longitude = d.location.longitude;
-      ip.countryName = d.registered_country.names.en;
-      ip.countryCode = d.registered_country.iso_code;
-      ip.timeZone = d.location.time_zone;
-      ip.zipCode = d.postal.code;
-      ip.regionName = d.subdivisions[0].names.en;
-      ip.connectionType = d.traits.connection_type;
-      ip.accuracy = d.location.accuracy_radius;
-    }).catch((e) => {
+    document.querySelectorAll("#update-storage").forEach(e => {
+      e.addEventListener("click", () => {
+        updateLocalStorage();
+      })
+    });
+    getGeoLocation().catch((e) => {
       ip = {
         "ipVersion": 4,
         "ipAddress": "Failed",
@@ -443,6 +519,9 @@ time div {
     theme = parseInt(localStorage.getItem("theme") || "1");
     localStorage.setItem("theme", theme.toString());
 
+    font = parseInt(localStorage.getItem("font") || "0");
+    localStorage.setItem("font", font.toString());
+
     if (localStorage.getItem("bg") !== null && localStorage.getItem("rgb") !== null) {
       hex = localStorage.getItem("bg")!;
       rgb = JSON.parse(localStorage.getItem("rgb")!);
@@ -461,11 +540,6 @@ time div {
       
       if (!(re.test(userAgent))) {
         document.title = `${timeStr} (${ip.cityName}, ${ip.countryCode}) | The Time`;
-      }
-      localStorage.setItem("theme", theme.toString());
-      if (theme === 2) {
-        localStorage.setItem("bg", hex);
-        localStorage.setItem("rgb", JSON.stringify(rgb));
       }
     }, 10);
     actuallyGetTime().then((time) => {
